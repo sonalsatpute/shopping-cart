014299b8fb77c23effed7f7df95efd43fb6c7ce4
sonal satpute <sonal.satpute@gmail.com>
2020-04-11 00:59:20 +0530

    initial commit

---
+++ src/test/kotlin/EnvironmentTests.kt
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class EnvironmentTests {
+    @Test
+    fun `environment should work`() {
+        assertTrue(true)
+    }
+}
